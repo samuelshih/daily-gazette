be24f9ac4931e00fd12cf733643817d6523a41e0
Samuel Shih <sam00429@gmail.com>
2015-12-16 20:22:44 -0500

    remove woodpecker, add colormag

--- wp-content/themes/woodpecker/searchform.php
+++
@@ -1,8 +0,0 @@
-<form role="search" method="get" class="searchform" action="<?php echo home_url('/'); ?>">
-    <div>
-        <input type="text" placeholder="<?php _e('Search','woodpecker') ?>" name="s" id="search" />
-        <input type="submit" id="searchsubmit" value="" />
-    </div>
-</form>
-<div class="clear"></div>
-<br/>
