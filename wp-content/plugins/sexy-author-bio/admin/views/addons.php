1972f9674e80da8a8e6b5c5e79cc0edb19485f65
Samuel Shih <sam00429@gmail.com>
2016-02-01 22:14:12 -0500

    sexy author bios

---
+++ wp-content/plugins/sexy-author-bio/admin/views/addons.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin addons view.
+ *
+ * @package   Sexy_Author_Bio
+ * @author    Andy Forsberg <andyforsberg@gmail.com>
+ * @license   GPL-2.0+
+ * @copyright 2014 Penguin Initiatives
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+?>
+
+<div class="wrap">
+
+	<h2><?php echo esc_html( get_addons_page_title() ); ?></h2>
+
+	<div>
+		<p>Check out these amazing addons!</p>
+	</div>
+
+</div>
