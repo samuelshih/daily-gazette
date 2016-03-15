2d3f43eeedbaf41d9cd9f09a5e9e9b248d8b39fc
Samuel Shih <sam00429@gmail.com>
2016-03-14 22:19:01 -0400

    testing coauthors

--- wp-content/themes/colormag/single.php
+++ wp-content/themes/colormag/single.php
@@ -25,13 +25,18 @@
 
       <?php get_template_part( 'navigation', 'single' ); ?>
 
-      <?php if ( get_the_author_meta( 'description' ) ) : ?>
-         <div class="author-box">
-            <div class="author-img"><?php echo get_avatar( get_the_author_meta( 'user_email' ), '100' ); ?></div>
-               <h4 class="author-name"><?php the_author_meta( 'display_name' ); ?></h4>
-               <p class="author-description"><?php the_author_meta( 'description' ); ?></p>
-         </div>
-      <?php endif; ?>
+			<?php if ( function_exists( 'coauthors_posts_links' ) ):
+    		coauthors_posts_links();
+			endif;
+			?>
+
+			<?php if ( get_the_author_meta( 'description' ) ) : ?>
+				 <div class="author-box">
+						<div class="author-img"><?php echo get_avatar( get_the_author_meta( 'user_email' ), '100' ); ?></div>
+							 <h4 class="author-name"><?php the_author_meta( 'display_name' ); ?></h4>
+							 <p class="author-description"><?php the_author_meta( 'description' ); ?></p>
+				 </div>
+			<?php endif; ?>
 
       <?php if ( get_theme_mod( 'colormag_related_posts_activate', 0 ) == 1 )
          get_template_part( 'inc/related-posts' );
